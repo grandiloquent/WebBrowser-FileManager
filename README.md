29697b6859a314fd5a1b0a2dd2944bc4d31714af
grandiloquent <1785907865@qq.com>
2023-03-20 06:37:21 +0800

    更新

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 - https://github.com/nlohmann/json
 
 ```
-http://192.168.8.189:8080/api/file?path=D%3A%5CBooks1&action=6&dst=D%3A%5CBooks&q=Excel
+http://192.168.8.189:8080/api/file?path=D:\\%E8%B5%84%E6%BA%90\Books&action=6&dst=D%3A%5CBooks&q=Postgre
 http://192.168.8.189:8080/api/file?path=D:\资源\Books&action=7
 C:\Users\Administrator\Desktop\Resources\Manager
 http://192.168.8.189:8080/api/file?action=9
--- editor.js
+++ editor.js
@@ -219,6 +219,224 @@ textarea {
     customDialog.style.display = 'none';
 })();
 
+(() => {
+    class CustomBottomSheet extends HTMLElement {
+
+        constructor() {
+            super();
+            this.attachShadow({
+                mode: 'open'
+            });
+            const wrapper = document.createElement("div");
+            wrapper.setAttribute("class", "wrapper");
+            const style = document.createElement('style');
+            style.textContent = `.icon {
+
+  display: inline-block;
+  flex-shrink: 0;
+  width: 24px;
+  height: 24px;
+  fill: currentColor;
+  stroke: none;
+  margin-right: 12px;
+
+}
+
+
+button {
+  padding: 0;
+  border: none;
+  outline: none;
+  font: inherit;
+  text-transform: inherit;
+  color: inherit;
+  background: transparent
+}
+
+button {
+
+  cursor: pointer;
+  box-sizing: border-box;
+  text-align: initial;
+  text-transform: unset;
+  width: 100%;
+  display: flex;
+  padding: 0;
+  margin-left: 12px;
+  font-size: 1.6rem;
+  line-height: 2.2rem;
+
+}
+
+.menu-item {
+
+  padding: 0;
+  height: 48px;
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  background:#fff;
+  font-size: 1.6rem;
+  line-height: 2.2rem;
+  justify-content:center;
+}
+
+.bottom-sheet-layout-content-wrapper {
+
+  -webkit-box-flex: 1;
+  flex: 1;
+  overflow-y: scroll;
+  /*max-height: 379.2px;*/
+
+    display: grid;
+    grid-template-columns: repeat(3,1fr);
+    background:#dadce0;
+    gap:1px;
+    border:1px solid #dadce0;
+}
+
+.bottom-sheet-layout-header-title-wrapper {
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  flex-direction: column;
+  display: flex;
+  margin-left: 12px
+}
+
+.bottom-sheet-layout-header {
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  display: flex;
+  margin-top: 8px
+}
+
+.bottom-sheet-drag-line {
+  background: #0f0f0f;
+  opacity: .15;
+  border-radius: 4px;
+  height: 4px;
+  margin: 0 auto;
+  width: 40px;
+  margin-top: 8px
+}
+
+.bottom-sheet-layout-header-wrapper {
+  overflow: hidden;
+  -webkit-box-flex: 0;
+  flex: none;
+  border-bottom: 1px solid #fff;
+}
+
+.bottom-sheet-layout {
+  border-radius: 12px;
+  background-color: #fff;
+  display: block;
+  overflow: hidden;
+  position: fixed;
+  margin: 0 8px 24px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 2
+}
+
+.overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+  cursor: pointer;
+  background-color: rgba(0, 0, 0, 0.6)
+}
+
+.wrapper {
+  position: fixed;
+  z-index: 5
+}`;
+            this.wrapper = wrapper;
+            this.shadowRoot.append(style, wrapper);
+        }
+
+        close() {
+            this.style.display = 'none';
+        }
+
+        click(evt) {
+            this.dispatchEvent(new CustomEvent('submit', {
+                detail: {
+                    id: evt.currentTarget.dataset.id
+                }
+            }));
+        }
+
+        set data(value) {
+
+
+            this.contentWrapper.insertAdjacentHTML('afterbegin', value.map(element => {
+                return `<div bind @click="click" data-id="${element.id}"  class="menu-item">${element.title}</div>`;
+            }).join(''));
+            /////////////////////
+            this.contentWrapper.querySelectorAll('[bind]').forEach(element => {
+                if (element.getAttribute('bind')) {
+                    this[element.getAttribute('bind')] = element;
+                }
+                [...element.attributes].filter(attr => attr.nodeName.startsWith('@')).forEach(attr => {
+                    if (!attr.value) return;
+                    element.addEventListener(attr.nodeName.slice(1), evt => {
+                        this[attr.value](evt);
+                    });
+                });
+            })
+        }
+
+        connectedCallback() {
+            this.wrapper.innerHTML = `<div bind @click="close" class="overlay">
+</div>
+<div class="bottom-sheet-layout">
+  <div class="bottom-sheet-layout-header-wrapper">
+    <div class="bottom-sheet-drag-line">
+    </div>
+    <div class="bottom-sheet-layout-header">
+      <div class="bottom-sheet-layout-header-title-wrapper">
+      </div>
+      <div bind="contentWrapper" class="bottom-sheet-layout-content-wrapper">
+        
+      </div>
+    </div>
+  </div>
+</div>`;
+            this.wrapper.querySelectorAll('[bind]').forEach(element => {
+                if (element.getAttribute('bind')) {
+                    this[element.getAttribute('bind')] = element;
+                }
+                [...element.attributes].filter(attr => attr.nodeName.startsWith('@')).forEach(attr => {
+                    if (!attr.value) return;
+                    element.addEventListener(attr.nodeName.slice(1), evt => {
+                        this[attr.value](evt);
+                    });
+                });
+            });
+
+
+            this.contentWrapper.innerHTML = `<div bind @click="close" class="menu-item">取消</div>`
+        }
+
+    }
+
+    customElements.define('custom-bottom-sheet', CustomBottomSheet);
+    /*
+    <!--
+                        <script type="module" src="./components/custom-bottom-sheet.js"></script>
+  <custom-bottom-sheet></custom-bottom-sheet>
+  customElements.whenDefined('custom-bottom-sheet').then(() => {
+  customBottomSheet.data = []
+  })
+  -->
+  */
+})();
+
 /*
 绑定元素和事件
 例如：<div bind="div" @click="click"></div>
@@ -315,6 +533,34 @@ function findCodeBlock(textarea) {
     }
     return [start, end];
 }
+
+function findCodeBlockExtend(textarea) {
+    const value = textarea.value;
+    let start = textarea.selectionStart;
+    let end = textarea.selectionEnd;
+    while (start > -1) {
+        if (value[start] === '`' && value[start - 1] === '`' && value[start - 2] === '`') {
+            // start += 1;
+            start -= 2;
+            while (start - 1 > 0 && value[start - 1] !== '\n') {
+                start--;
+
+            }
+
+            break;
+        }
+        start--;
+    }
+    while (end < value.length) {
+        if (value[end] === '`' && value[end + 1] === '`' && value[end + 2] === '`') {
+            end += 2;
+            break;
+        }
+        end++;
+    }
+    return [start, end];
+}
+
 
 function findExtendPosition(editor) {
     const start = editor.selectionStart;
@@ -765,6 +1011,9 @@ customElements.whenDefined('custom-bottom-sheet').then(() => {
     }, {
         id: 6,
         title: "执行代码"
+    }, {
+        id: 8,
+        title: "插入链接"
     }]
 })
 
@@ -791,6 +1040,9 @@ function onCustomBottomSheet(evt) {
             break;
         case "7":
             customDialog.style.display = 'block';
+            break;
+        case "8":
+            insertLink()
             break;
     }
 }
@@ -835,7 +1087,7 @@ document.addEventListener('keydown', async evt => {
             textarea.setRangeText(await navigator.clipboard.readText(), p[0], p[1], "end");
         } else if (evt.key === '3') {
             evt.preventDefault();
-            const p = findCodeBlock(textarea);
+            const p = findCodeBlockExtend(textarea);
             textarea.setRangeText(textarea.value.substring(p[0], p[1])
                 .split('\n')
                 .map(x => `    ${x.trimEnd()}`).join('\n'), p[0], p[1]);
