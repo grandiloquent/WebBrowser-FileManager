ce809bf8e3d82b9be40e7fb90f6b7d884606b810
grandiloquent <1785907865@qq.com>
2023-04-06 16:57:38 +0800

    更新

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::config::LogLevel;
+use rocket::data::{Limits, ToByteUnit};
+use rocket::figment::Figment;
+use local_ip_address::local_ip;
+
+//noinspection RsMainFunctionNotFound
+#[launch]
+fn rocket() -> _ {
+
+    let my_local_ip = local_ip().unwrap();
+
+    let limits = Limits::default()
+        .limit("json", 3.mebibytes())
+        .limit("string", 3.mebibytes())
+        .limit("file", 5.gibibytes());
+
+    let figment = Figment::from(rocket::Config::default())
+        .merge((rocket::Config::ADDRESS, my_local_ip))
+        .merge((rocket::Config::PORT, 3000))
+        .merge((rocket::Config::TEMP_DIR, ""))
+        .merge((rocket::Config::LIMITS, limits))
+        .merge((rocket::Config::LOG_LEVEL, LogLevel::Critical));
+
+    rocket::custom(figment)
 }
+
