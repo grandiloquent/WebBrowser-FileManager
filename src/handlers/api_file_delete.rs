629dd8639561bfc4182c2f0d2af6ce31e25d2988
grandiloquent <1785907865@qq.com>
2023-04-06 23:21:32 +0800

    更新

--- src/handlers/api_file_delete.rs
+++ src/handlers/api_file_delete.rs
@@ -5,31 +5,29 @@ use rocket::futures::future::err;
 use rocket::http::Status;
 use rocket::serde::{json::Json};
 use rocket::State;
+
+fn remove_file(path: &Path) -> Result<(), std::io::Error> {
+    if path.is_dir() {
+        fs::remove_dir_all(path)
+    } else {
+        fs::remove_file(path)
+    }
+}
+
 #[post("/api/file/delete", data = "<list>")]
 pub async fn api_file_delete(list: Json<Vec<String>>) -> Result<(), Status> {
 // https://doc.rust-lang.org/std/path/struct.Path.html
     for path in list.into_inner() {
         let p = Path::new(path.as_str());
-        if p.is_dir() {
-            // https://doc.rust-lang.org/stable/std/fs/fn.remove_dir_all.html
-            return match fs::remove_dir_all(p) {
-                Ok(_) => {
-                    Ok(())
-                }
-                Err(error) => {
-                    Err(Status::NotFound)
-                }
-            };
-        } else {
-            return match fs::remove_file(p) {
-                Ok(_) => {
-                    Ok(())
-                }
-                Err(error) => {
-                    Err(Status::NotFound)
-                }
-            };
-        }
+
+        // https://doc.rust-lang.org/stable/std/fs/fn.remove_dir_all.html
+        match remove_file(p) {
+            Ok(()) => continue,
+            Err(error) => {
+                println!("{}", error);
+                return Err(Status::NotFound);
+            }
+        };
     }
     Ok(())
 }
