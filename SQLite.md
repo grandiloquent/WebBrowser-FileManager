a43c51bce21777c5e0b8d8df8a814431f7f7c136
grandiloquent <1785907865@qq.com>
2023-04-10 19:08:39 +0800

    增加SQLite

---
+++ SQLite.md
@@ -0,0 +1,4 @@
+https://github.com/SergioBenitez/Rocket/tree/master/examples/databases
+https://github.com/rusqlite/rusqlite
+http://docs.rs/rusqlite/
+rusqlite = "0.29.0"
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,14 @@ use rocket::config::LogLevel;
 use rocket::data::{Limits, ToByteUnit};
 use rocket::figment::Figment;
 use local_ip_address::local_ip;
+use rocket::{Build, Config, Rocket};
+use rocket::figment::providers::{Format, Toml};
+use rusqlite::Connection;
+
+use rocket_sync_db_pools::{database, diesel};
+
+#[database("notes")]
+struct NotesConnection(diesel::SqliteConnection);
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
@@ -22,15 +30,21 @@ async fn main() -> Result<(), rocket::Error> {
         .limit("json", 3.mebibytes())
         .limit("string", 3.mebibytes())
         .limit("file", 5.gibibytes());
-
+    let toml = Toml::string(r#"
+    [default.databases]
+    notes = { url = "notes.db", pool_size = 1 }
+     "#).nested();
     let figment = Figment::from(rocket::Config::default())
         .merge((rocket::Config::ADDRESS, my_local_ip))
         .merge((rocket::Config::PORT, 3000))
         .merge((rocket::Config::TEMP_DIR, ""))
         .merge((rocket::Config::LIMITS, limits))
-        .merge((rocket::Config::LOG_LEVEL, LogLevel::Critical));
+
+        .merge((rocket::Config::LOG_LEVEL, LogLevel::Critical))
+        .merge(toml);
 
     rocket::custom(figment)
+        .attach(NotesConnection::fairing())
         .mount("/",
                routes![handlers::api_asset_file::api_asset_file,handlers::api_file::api_file,handlers::api_file::api_file_post,handlers::api_files::api_files,handlers::api_file_delete::api_file_delete,handlers::api_file_new_dir::api_file_new_dir,handlers::api_file_new_file::api_file_new_file,handlers::auto_it::auto_it,handlers::compress_zip::compress_zip,handlers::editor::index,handlers::extract_zip::api_zip,handlers::favicon::favicon,handlers::file::file,handlers::index::index,handlers::rust::wasm,handlers::rust::rustfmt,handlers::video::videos,handlers::video::video,handlers::video::subtitle])
         .register("/", catchers![   handlers::not_found::not_found])
