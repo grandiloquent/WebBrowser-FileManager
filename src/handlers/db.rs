c4d016e309534a148683c3e1ad24e2fdc64ce1ac
grandiloquent <1785907865@qq.com>
2023-04-10 21:16:53 +0800

    增加SQLite

--- src/handlers/db.rs
+++ src/handlers/db.rs
@@ -3,36 +3,47 @@ use rocket::http::Status;
 use rocket::serde::json::serde_json;
 use rocket::serde::Serialize;
 use diesel::{self, result::QueryResult, prelude::*};
+
 mod schema {
     diesel::table! {
         notes(_id) {
             _id ->  Nullable<Integer>,
             title ->  Text,
-            content ->  Text,
+            content ->  Nullable<Text>,
             create_at ->  BigInt,
             update_at ->  BigInt,
         }
     }
 }
+
 use self::schema::notes;
 use self::schema::notes::dsl::{notes as all_notes};
+
 #[derive(Serialize, Queryable, Insertable, Debug, Clone)]
 #[serde(crate = "rocket::serde")]
 #[table_name = "notes"]
 pub struct Notes {
     pub _id: Option<i32>,
     pub title: String,
-    pub content: String,
+    pub content: Option<String>,
     pub create_at: i64,
     pub update_at: i64,
 }
+
 impl Notes {
     pub async fn all(conn: &NotesConnection) -> QueryResult<Vec<Notes>> {
         conn.run(|c| {
-            notes::table.order(notes::_id.desc()).load::<Notes>(c)
+            notes::table
+                .select((
+                    notes::_id,
+                    notes::title,
+                    notes::update_at,
+                ))
+                .order(notes::update_at.desc()).get_result(c)
         }).await
     }
 }
+
 #[get("/api/notes")]
 pub async fn get_notes(conn: NotesConnection) -> Result<String, Status> {
     match Notes::all(&conn).await {
